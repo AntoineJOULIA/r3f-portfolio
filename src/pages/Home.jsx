c7ce63d449c6742472a7b4a03d99c2b3711f17c7
Antoine Joulia <antoine.joulia@onera.fr>
2023-11-28 15:01:32 +0100

    Add sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -29,6 +30,7 @@ const Home = () => {
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
+          <Sky />
           <Island position={islandPosition} scale={islandScale} rotation={islandRotation} />
         </Suspense>
       </Canvas>
