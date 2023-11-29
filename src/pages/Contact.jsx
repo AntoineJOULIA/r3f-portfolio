d588938350cdbcd5f43abe602f1e267f467573b8
Antoine Joulia <antoine.joulia@onera.fr>
2023-11-29 15:34:29 +0100

    Contact Alert

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,12 +4,16 @@ import emailjs from "@emailjs/browser";
 import Loader from "../components/Loader";
 
 import Fox from "../models/Fox";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
   const [currentAnimation, setCurrentAnimation] = useState("idle");
+  const { alert, showAlert, hideAlert } = useAlert();
+  console.log(alert);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -35,10 +39,10 @@ const Contact = () => {
       )
       .then(() => {
         setIsLoading(false);
-        // TODO: show success message
-        // TODO: Hide an alert
+        showAlert({ show: true, text: "Message sent successfully", type: "success" });
 
         setTimeout(() => {
+          hideAlert();
           setCurrentAnimation("idle");
           setForm({ name: "", email: "", message: "" });
         }, 3000);
@@ -47,7 +51,7 @@ const Contact = () => {
         setIsLoading(false);
         setCurrentAnimation("idle");
         console.error(error);
-        // TODO: Show error message
+        showAlert({ show: true, text: "I didn't receive your message", type: "danger" });
       });
   };
 
@@ -57,6 +61,7 @@ const Contact = () => {
 
   return (
     <section className="relative flex flex-col lg:flex-row max-container">
+      {alert.show && <Alert {...alert} />}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in touch</h1>
         <form action="" className="flex flex-col w-full gap-7 mt-14" onSubmit={handleSubmit}>
