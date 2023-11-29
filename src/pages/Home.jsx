f36c7a6d04406f5b572eb8cc787a7daad8a10cc6
Antoine Joulia <antoine.joulia@onera.fr>
2023-11-29 17:04:30 +0100

    Add sound

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,28 @@ import Bird from "../models/Bird";
 import { Plane } from "@react-three/drei";
 import { useState } from "react";
 import HomeInfo from "../components/HomeInfo";
+import sakura from "../assets/sakura.mp3";
+import { useRef } from "react";
+import { soundoff, soundon } from "../assets/icons";
+import { useEffect } from "react";
 
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1);
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+
+  useEffect(() => {
+    if (isPlayingMusic) {
+      audioRef.current.play();
+    }
+
+    return () => {
+      audioRef.current.pause();
+    };
+  }, [isPlayingMusic]);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -70,6 +88,15 @@ const Home = () => {
           <Plane position={planePosition} scale={planeScale} rotation={[0, 20, 0]} isRotating={isRotating} />
         </Suspense>
       </Canvas>
+
+      <div className="absolute bottom-2 left-2">
+        <img
+          src={!isPlayingMusic ? soundoff : soundon}
+          alt="sound"
+          className="object-contain w-10 h-10 cursor-pointer"
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+        />
+      </div>
     </section>
   );
 };
